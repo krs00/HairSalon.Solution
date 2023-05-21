4193ba102c0450125506bc8dfa54d08c179ef0e9
Kymani Stephens <kymanist00@gmail.com>
2023-05-20 20:26:25 -0700

    add controller boilerplate

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      // List<Category> model = _db.Categories.ToList();
+      return View();
+    }
+
+
+  }
+}
---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      // List<Category> model = _db.Categories.ToList();
+      return View();
+    }
+
+
+  }
+}
