fa575e17ce793e8e79fb4a4c0b9c948f6b64c7d3
Kymani Stephens <kymanist00@gmail.com>
2023-05-20 21:43:48 -0700

    fix typo

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,7 +7,7 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
-    public string Specialty { get; set; }
+    public string Speciality { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public List <Client> Clients { get; set; } // NAVIGATION PROPERTY
