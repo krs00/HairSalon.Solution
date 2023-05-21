7199ee6284efc9ad2231903c6d74ad0191cbb676
Kymani Stephens <kymanist00@gmail.com>
2023-05-20 22:04:53 -0700

    configure stylist properties

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,9 +7,9 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
-    public string Speciality { get; set; }
     public string Phone { get; set; }
     public string Email { get; set; }
+    public string Speciality { get; set; }
     public List <Client> Clients { get; set; } // NAVIGATION PROPERTY
   }
 }
