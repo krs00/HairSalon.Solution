ccb2a6806d6e26cf06b84752f081a25d0588510a
Kymani Stephens <kymanist00@gmail.com>
2023-05-22 20:13:39 -0700

    add validation attribute

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,13 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HairSalon.Models
 {
-  public class Client
-  {
-    public int ClientId { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Phone { get; set; }
-    public string Email { get; set; }
-    public int StylistId { get; set; } // FOREIGN KEY PROPERTY!!
-    public Stylist Stylist { get; set; } // REFERENCE NAVIGATION PROPERTY!!
-  }
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "You must add a stylist before you add any clients!")]
+        public int StylistId { get; set; } // FOREIGN KEY PROPERTY!!
+
+        public Stylist Stylist { get; set; } // REFERENCE NAVIGATION PROPERTY!!
+    }
 }
